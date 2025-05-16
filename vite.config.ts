831fb85f3e7b55d727b262ef20c481c096df4ea3
CfM47 <jossuearteche000@gmail.com>
2025-05-16 12:23:50 -0400

    Added some configurations

--- vite.config.ts
+++ vite.config.ts
@@ -7,17 +7,25 @@ export default defineConfig({
   plugins: [react()],
   build: {
     copyPublicDir: false,
+    emptyOutDir: false,
     lib: {
       entry: resolve(__dirname, "lib/index.ts"),
       name: "ComponentLibrary",
-      fileName: "component-library",
-      formats: ["es"],
+      fileName: (format) => `index.${format}.js`,
+      formats: ["es", "cjs"],
     },
     rollupOptions: {
-      external: ["react"],
+      external: [
+        "react",
+        "react-dom",
+        "@emotion/react",
+        "@emotion/styled",
+        "@mui/material",
+      ],
       output: {
         globals: {
           react: "react",
+          "react-dom": "ReactDOM",
         },
       },
     },
